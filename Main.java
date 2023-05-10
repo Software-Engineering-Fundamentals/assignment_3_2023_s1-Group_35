6fd17467126b28d5d73574631e778b1b0dfb87df
Nathan Nguyen <103355606+notnathann@users.noreply.github.com>
2023-05-10 20:04:18 +1000

    Added skeleton comments

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+              // Instantiate classes
+              // Trainee lodges a registration fee 
+              // Finance manager checks if the trainee is eligible
+              // Lodge a feedback/complaint
     }
 }
