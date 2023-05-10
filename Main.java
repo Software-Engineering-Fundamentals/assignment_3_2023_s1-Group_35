48b2a0396d37d2eaa57a2b9a055f455a01bf6986
Nathan Nguyen <103355606+notnathann@users.noreply.github.com>
2023-05-10 20:04:55 +1000

    Instantiate classes (to be added)

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-              // Instantiate classes
+              // Instantiate classes (to be added)
+              Trainee trainee = new Trainee();
+              FinanceManager financeManager = new FinanceManager();
+              Manager manager = new Manager();
+              CourseFeedback courseFeedback = new CourseFeedback();
+
               // Trainee lodges a registration fee 
               // Finance manager checks if the trainee is eligible
               // Lodge a feedback/complaint
