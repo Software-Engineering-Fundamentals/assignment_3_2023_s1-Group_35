d8fe5c32959af122b6487785014d69863f4e8a7c
Nathan Nguyen <103355606+notnathann@users.noreply.github.com>
2023-05-10 20:09:36 +1000

    Corrected indenting and updated comments. Fin main

--- Main.java
+++ Main.java
@@ -4,28 +4,28 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-              // Instantiate classes (to be added)
-              Trainee trainee = new Trainee();
-              FinanceManager financeManager = new FinanceManager();
-              Manager manager = new Manager();
-              CourseFeedback courseFeedback = new CourseFeedback();
+        // Instantiate classes
+        Trainee trainee = new Trainee();
+        FinanceManager financeManager = new FinanceManager();
+        Manager manager = new Manager();
+        CourseFeedback courseFeedback = new CourseFeedback();
 
-              // Trainee lodges a registration fee 
-              trainee.requestRefund();
+        // Trainee lodges a registration fee refund request which is addressed by the finance manager
+        trainee.requestRefund();
+		
+        // Finance manager checks if the trainee is eligible for a refund
+        boolean eligibleForRefund = true; // Dummy value, real implementation should check the eligibility
+        if (eligibleForRefund) {
+            financeManager.payRefund();
+            System.out.println("Refund has been paid to the trainee.");
+        } else {
+            System.out.println("Refund request denied.");
+        }
 
-              // Finance manager checks if the trainee is eligible
-              boolean eligibleForRefund = true; // Dummy value, real implementation should check the eligibility
-              if (eligibleForRefund) {
-                  financeManager.payRefund();
-                  System.out.println("Refund has been paid to the trainee.");
-              } else {
-                  System.out.println("Refund request denied.");
-              }
-
-              // Lodge a feedback/complaint
-              trainee.provideCourseFeedback();
-              manager.accessFeedback();
-              courseFeedback.accessFeedbackNotes();
-              System.out.println("Feedback/Complaint has been successfully addressed by the Manager.");
+        // Lodge a feedback/complaint which is successfully addressed by the Manager
+        trainee.provideCourseFeedback();
+        manager.accessFeedback();
+        courseFeedback.accessFeedbackNotes();
+        System.out.println("Feedback/Complaint has been successfully addressed by the Manager.");
     }
 }
