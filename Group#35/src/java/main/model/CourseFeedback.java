e50a11a63d2fa5e712ce17a48b21faaf4c9e760d
Ruschia <s3849216@student.rmit.edu.au>
2023-05-14 01:08:14 +1000

    Created CourseFeedback class

---
+++ Group#35/src/java/main/model/CourseFeedback.java
@@ -0,0 +1,16 @@
+package model;
+
+class CourseFeedback {
+
+    int feedbackID;
+    String feedbackName;
+    String feedbackNotes;
+
+    
+    void accessFeedbackNotes() {
+    
+    // Access the feedback notes
+    
+    }
+    
+}
