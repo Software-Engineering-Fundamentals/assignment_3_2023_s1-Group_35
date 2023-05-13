33cabb29dc66db5e3141e6ab124ffa4b84bf310a
Ruschia <s3849216@student.rmit.edu.au>
2023-05-14 01:05:13 +1000

    Created Trainee class

---
+++ Group#35/src/java/main/model/Trainee.java
@@ -0,0 +1,18 @@
+package model;
+
+class Trainee extends GenericUser {
+
+    void requestRefund() {
+    
+    // Request refund from the finance manager
+    
+    }
+    
+    
+    void provideCourseFeedback() {
+    
+    // Provide course feedback to the manager
+    
+    }
+    
+    }
